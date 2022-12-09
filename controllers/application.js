0626c38561351be85a511d004154690fce66cd0c
HamadZafarullah <hamadzafarullah@gmail.com>
2022-12-08 20:55:39 -0500

    working on displaying ID

--- controllers/application.js
+++ controllers/application.js
@@ -14,54 +14,8 @@ router.get('/', async (req, res) => {
     }
 
 });
-router.get("/:id", async (req, res) => {
-  try {
 
-      res.json(await Travelapp.findById(req.params.id));
-    } catch (error) {
 
-      res.status(400).json(error);
-    }
-});
-// // Get all attraction
-// router.get('/attraction/atrraction:', async (req, res, next) => {
-//     try {
-//       const travelapp = await travelapp.find({make: req.params.attraction})
-//       res.json(travelapp);
-//     } catch (err) {
-//           next(err);
-//     }
-// });
-
-// // Get all information
-// router.get('/information/:information', async (req, res, next) => {
-//     try {
-//       const travelapp = await travelapp.find({model: req.params.information})
-//       res.json(travelapp);
-//     } catch (err) {
-//           next(err);
-//     }
-// });
-
-// // Get all cost
-// router.get('/cost/:cost', async (req, res, next) => {
-//     try {
-//       const travelapp = await travelapp.find({cost: { $gte: req.params.cost}})
-//       res.json(travelapp);
-//     } catch (err) {
-//           next(err);
-//     }
-// });
-
-// // Get all image
-// router.get('/image/image', async (req, res, next) => {
-//     try {
-//       const travelapp = await travelapp.find({model: req.params.image})
-//       res.json(travelapp.image);
-//     } catch (err) {
-//           next(err);
-//     }
-// });
 
 
 module.exports = router
