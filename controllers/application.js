6ad1b25518599608b0fa04bd123a2d1bc77f812d
HamadZafarullah <hamadzafarullah@gmail.com>
2022-12-12 20:27:42 -0500

    added create-update-delete route

--- controllers/application.js
+++ controllers/application.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Travelapp = require('../models/Travelapp')
 
 
-
+//home route
 router.get('/', async (req, res) => {
     try {
       res.status(200).json(await Travelapp.find({}))
@@ -14,7 +14,18 @@ router.get('/', async (req, res) => {
     }
 
 });
+//create route
+router.post("/", async (req, res) => {
+  try {
 
+    res.json(await Travelapp.create(req.body));
+  } catch (error) {
+    //send error
+
+    res.status(400).json(error);
+  }
+});
+//locationID route
 router.get("/:id", async (req, res) => {
   try {
 
@@ -23,6 +34,29 @@ router.get("/:id", async (req, res) => {
 
       res.status(400).json(error);
     }
+});
+// Location UPDATE ROUTE
+router.put("/:id", async (req, res) => {
+  try {
+    // update people by ID
+    res.json(
+      await Travelapp.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
+});
+
+// Location DELETE ROUTE
+router.delete("/:id", async (req, res) => {
+  try {
+    // delete location by ID
+    res.json(await Travelapp.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
 });
 
 
