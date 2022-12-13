223d14690a28c0b778167631fa2499e6d3c99bcb
sky0815a <sky0815a@gmail.com>
2022-12-13 09:46:28 -0500

    seed.js fixed

--- db/seed.js
+++ db/seed.js
@@ -3,24 +3,8 @@ const mongoose = require('./connection');
 const Travelapp = require('../models/Travelapp');
 const travelappSeeds = require("../SampleData/location.json");
 
-// Travelapp.deleteMany({}).then(() => {
-//     Travelapp.create(travelappSeeds).then(travelapp => {
-//     })
-// })
-
-const seedinData = async () => {
-    try {
-
-        let allData = travelappSeeds;
-
-        console.log('hello', allData);
-
-        const addedData = Travelapp.insertMany(allData);
-
-        console.log(addedData);
-    } catch(err) {
-        console.log(err);
-    }
-}
-seedinData();
-
+Travelapp.remove({}).then(() => {
+  return Travelapp.collection.insert(travelappSeeds)
+}).then(() => {
+  process.exit()
+})
